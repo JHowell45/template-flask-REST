3daa50da2ad1ecb58da841a69a2962d55890e8ef
JHowell45 <jhowell45@googlemail.com>
2019-06-27 21:10:17 +0100

    Created instance of the restplus instance for creating endpoints for the Flask app.

---
+++ app/extensions/__init__.py
@@ -0,0 +1,6 @@
+"""Use this file for importing and creating instances for all of the extensions.
+
+This file contains the instances of all of the extensions required for this Flask
+application.
+"""
+from .restplus_extension import api
