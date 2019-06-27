8501399f29a5a82c140f3f0bd1626c1e189deccf
JHowell45 <jhowell45@googlemail.com>
2019-06-27 21:11:12 +0100

    Created config file for the Flask application.

---
+++ config.py
@@ -0,0 +1,9 @@
+"""Use this class for defining the configurations for the backend Flask application.
+
+This file contains the classes used for defining the configurations for the Flask
+application and its extensions.
+"""
+
+
+class Config:
+    pass
